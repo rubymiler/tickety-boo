585f0dc7298319caa4ba25239db5c5e55ff18a7c
github username <lushiyun@outlook.com>
2020-07-07 18:28:40 -0400

    Customize user signup, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   root 'pages#home'
 end
