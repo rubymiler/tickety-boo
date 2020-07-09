befbaefe7018570cb48a7c1417f0ee04f65b3dd2
github username <lushiyun@outlook.com>
2020-07-09 13:54:33 -0400

    Add user scoped root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   resources :tickets
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, path: '',
+                     path_names: {
+                       sign_in: 'login',
+                       sign_out: 'logout',
+                       sign_up: 'signup'
+                      },
+                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  # TODO: add user_root_path
+  devise_scope :user do
+    root 'tickets#index', as: :user_root
+  end
 
   root 'pages#home'
 end
