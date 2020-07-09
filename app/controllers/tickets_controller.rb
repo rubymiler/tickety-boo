942698e30a023b43c8508a3e4ac8ab924cc67c79
github username <lushiyun@outlook.com>
2020-07-09 15:34:37 -0400

    Add edit and update function for tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -15,15 +15,28 @@ class TicketsController < ApplicationController
     @ticket = current_user.submitted_tickets.create(ticket_params)
 
     if @ticket.save
-      redirect_to ticket_path(@ticket), notice: 'Ticket created successfully.'
+      redirect_to @ticket, notice: 'Ticket created successfully.'
     else
       render :new, alert: 'Could not create your ticket.'
+    end
+  end
+
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    if @ticket.update(ticket_params)
+      redirect_to @ticket, notice: 'Ticket updated successfully.'
+    else
+      render :edit, alert: 'Could not update ticket.'
     end
   end
 
   private
 
   def ticket_params
-    params.require(:ticket).permit(:id, :title, :description)
+    params.require(:ticket).permit(:title, :description)
   end
 end
