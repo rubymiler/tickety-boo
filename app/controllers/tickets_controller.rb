3339eac3e961d8f1a7be806afceee67b8436989c
github username <lushiyun@outlook.com>
2020-07-09 13:55:08 -0400

    Add ticket index and show function

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,9 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all.public
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :submitter, class_name: 'User'
+
+  def self.public
+    where(public: true)
+  end
 end
