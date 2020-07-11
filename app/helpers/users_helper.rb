b13046e557fc222485f014dd91d7518fd3941c8c
github username <lushiyun@outlook.com>
2020-07-11 19:26:41 -0400

    Fix avatar variant

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def show_avatar(resource)
     if resource.avatar.attached?
-      image_tag(resource.avatar_url, class: 'avatar-img')
+      image_tag(resource.avatar.variant(resize: '100x100!'), class: 'avatar-img')
     elsif resource.avatar_url
       image_tag(resource.avatar_url, class: 'avatar-img')
     else
