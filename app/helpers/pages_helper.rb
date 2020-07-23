6d9e3a0091f3c49b613b1bda72a8b5541251a8ee
github username <lushiyun@outlook.com>
2020-07-23 00:44:01 -0400

    Refactor header to helpers and shared views

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,17 @@
 module PagesHelper
+  def links_container
+    if user_signed_in?
+      render 'shared/signed_in_nav_links'
+    else
+      render 'shared/logged_out_nav_links'
+    end
+  end
+
+  def action_container
+    if user_signed_in?
+      render 'shared/signed_in_nav_actions'
+    else
+      render 'shared/logged_out_nav_actions'
+    end
+  end
 end
