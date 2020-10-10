adf92fe1b688e4681c06d433a8e9ce03760e65bd
github username <lushiyun@outlook.com>
2020-10-10 19:33:46 -0400

    Add encrypted sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => 'tickety-boo.herokuapp.com' }
-
   config.action_mailer.default_options = {from: 'ticketyboo.dev@gmail.com'}
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'ticketyboo.dev@gmail.com',
-    password:             Rails.application.credentials.dig(:gmail, :password),
-    authentication:       'plain',
-    enable_starttls_auto: true }
+    :user_name => Rails.application.credentials.dig(:send_grid, :USERNAME),
+    :password => Rails.application.credentials.dig(:send_grid, :PASSWORD),
+    :domain => 'gmail.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
